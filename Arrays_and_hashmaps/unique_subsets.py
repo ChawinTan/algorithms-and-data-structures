f6392b84e6c32142565c9339b4e85b4464c6460a
ChawinTan <chawin15@gmail.com>
2019-01-28 20:49:26 +0800

    backtracking for subsets

--- Arrays_and_hashmaps/unique_subsets.py
+++ Arrays_and_hashmaps/unique_subsets.py
@@ -12,3 +12,23 @@ class Solution:
                 
         dfs([], nums)
         return res
+
+
+''' backtracking '''
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        def backtrack(path, nums, i):
+            res.append(path[::])
+            
+            for i in range(len(nums)):
+                path.append(nums[i])
+                backtrack(path, nums[i+1:], i)
+                path.pop()
+
+        backtrack([], nums, 0)
+        return res
